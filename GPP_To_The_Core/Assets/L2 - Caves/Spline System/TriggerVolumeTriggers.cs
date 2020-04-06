cedf955d71602fbbb6cc77a21f24c429c5e72eb1
Conrad Wilson <conradjwilson@hotmail.com>
2020-04-06 18:01:35 +0100

    Added Spline System
    To use: Drag 2.5D Spline prefab into scene, unpack prefab, then press 'Setup/Reset Spline' button to set it up.
    Add more waypoints/curves with the 'Add Waypoint button'.
    Reset the spline with the reset button again.
    You can add things to do when something enters the trigger volumes on each end of the spline within the 'TriggerVolumeTriggers' script. Example code has been commented out.

---
+++ GPP_To_The_Core/Assets/L2 - Caves/Spline System/TriggerVolumeTriggers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerVolumeTriggers : MonoBehaviour
+{
+    public bool canEnter;
+    private Collider playerCol;
+
+    private void Start()
+    {
+        canEnter = true;
+        playerCol = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (canEnter && other == playerCol && transform.name == "Start Trigger Volume")
+        {
+            canEnter = false;
+            //gameObject.GetComponentInParent<SplinePlayerController>().BeginEnterSpline(true);
+        }
+        else if (canEnter && other == playerCol && transform.name == "End Trigger Volume")
+        {
+            canEnter = false;
+            //gameObject.GetComponentInParent<SplinePlayerController>().BeginEnterSpline(false);
+        }
+        else if (!canEnter && other == playerCol)
+        {
+            //gameObject.GetComponentInParent<SplinePlayerController>().BeginExitSpline();
+        }
+    }
+}
