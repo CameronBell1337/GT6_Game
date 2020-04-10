ddcb55af837baa48f7c1d905ec99a31aa038e022
Zoe <zoerowbotham1@gmail.com>
2020-04-10 13:13:15 +0100

    Minecart Leaning

---
+++ GPP_To_The_Core/Assets/L2 - Caves/Scripts/MinecartTrigger.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class MinecartTrigger : MonoBehaviour
+{
+    public Transform playerPositionTarget;
+    public Transform playerExitPosition;
+    
+    bool playerInRange = false;
+
+    TrackController trackController;
+    GameObject player;
+    Animator minecartAnimator;
+
+    public float tiltSpeed = 3;
+    Vector3 maxTiltPosition = new Vector3(0, 0.15f, 0);
+    Vector3 maxTiltRotation = new Vector3(0, 0, 20);
+
+    private void Awake()
+    {
+        trackController = GetComponentInParent<TrackController>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        minecartAnimator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("AttackL") && playerInRange && !trackController.active)
+        {
+            if (player.transform.parent == null)
+            {
+                trackController.active = true;
+                player.transform.position = playerPositionTarget.position;
+                player.transform.parent = transform;
+            }
+            else
+            {
+                player.transform.parent = null;
+                player.transform.position = playerExitPosition.position;
+            }
+        }
+
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            trackController.tiltPositionOffset = Vector3.Lerp(trackController.tiltPositionOffset, maxTiltPosition, tiltSpeed * Time.deltaTime);
+            trackController.tiltRotationOffset = Vector3.Lerp(trackController.tiltRotationOffset, maxTiltRotation * trackController.direction, tiltSpeed * Time.deltaTime);
+        }
+        else if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            trackController.tiltPositionOffset = Vector3.Lerp(trackController.tiltPositionOffset, maxTiltPosition, tiltSpeed * Time.deltaTime);
+            trackController.tiltRotationOffset = Vector3.Lerp(trackController.tiltRotationOffset, -maxTiltRotation * trackController.direction, tiltSpeed * Time.deltaTime);
+        }
+        else
+        {
+            trackController.tiltPositionOffset = Vector3.Lerp(trackController.tiltPositionOffset, Vector3.zero, tiltSpeed * Time.deltaTime);
+            trackController.tiltRotationOffset = Vector3.Lerp(trackController.tiltRotationOffset, Vector3.zero, tiltSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ GPP_To_The_Core/Assets/L2 - Caves/Scripts/TrackController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class TrackController : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public Transform minecart;
+
+    public bool startMinecartAtBeginning;
+    public float minecartMoveSpeed;
+
+    VertexPath path;
+
+    public bool active = false;
+
+    [HideInInspector]
+    public int direction = 0;
+    float distanceTravelled = 0;
+
+    public Vector3 tiltPositionOffset = Vector3.zero;
+    public Vector3 tiltRotationOffset = Vector3.zero;
+
+    private void Awake()
+    {
+        path = pathCreator.path;
+
+        if (startMinecartAtBeginning)
+        {
+            distanceTravelled = 0;
+            direction = 1;
+        }
+        else
+        {
+            distanceTravelled = path.length - 0.5f;
+            direction = -1;
+        }
+
+        MoveMinecart();
+    }
+
+    private void Update()
+    {
+        if (active)
+        {
+            distanceTravelled += Time.deltaTime * minecartMoveSpeed * direction;
+            MoveMinecart();
+        }
+    }
+
+    private void StopMinecart()
+    {
+        active = false;
+        direction *= -1;
+        tiltPositionOffset = Vector3.zero;
+        tiltRotationOffset = Vector3.zero;
+
+        minecart.position = path.GetPointAtDistance(distanceTravelled);
+        Vector3 rotation = path.GetRotationAtDistance(distanceTravelled).eulerAngles;
+        rotation.z = 0;
+        minecart.rotation = Quaternion.Euler(rotation);
+    }
+
+    private void MoveMinecart()
+    {
+        minecart.position = path.GetPointAtDistance(distanceTravelled) + tiltPositionOffset;
+        Vector3 rotation = path.GetRotationAtDistance(distanceTravelled).eulerAngles;
+        rotation.z = tiltRotationOffset.z;
+        minecart.rotation = Quaternion.Euler(rotation);
+
+        if (direction > 0)
+        {
+            if (Vector3.Distance(path.GetPointAtDistance(distanceTravelled), path.GetPointAtDistance(path.length - 0.5f)) < 0.5f)
+            {
+                StopMinecart();
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(path.GetPointAtDistance(distanceTravelled), path.GetPointAtDistance(0)) < 0.5f)
+            {
+                StopMinecart();
+            }
+        }
+    }
+}
