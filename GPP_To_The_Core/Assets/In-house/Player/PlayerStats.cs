84622ca66bfcee69eef524c0be93191d7b326fb4
Conrad Wilson <conradjwilson@hotmail.com>
2020-04-19 21:04:41 +0100

    Added death animation to death state (0 health)
    +Added animation states & new parameters to transition from any state into death, then from death to idle once revived.
    *Implemented better way of tracking health reaching 0, not setting to -1 but using a bool to check if should keep checking for health reaching 0 instead and in case of under 0 values.

--- GPP_To_The_Core/Assets/In-house/Player/PlayerStats.cs
+++ GPP_To_The_Core/Assets/In-house/Player/PlayerStats.cs
@@ -7,12 +7,14 @@ public class PlayerStats : MonoBehaviour
     /*[HideInInspector]*/ public float health;
 
     [SerializeField] private float maxHeath;
+    private Animator anim;
     private PlayerInput input;
     private PlayerMovement movementScript;
 
     void Start()
     {
         health = maxHeath;
+        anim = GetComponent<Animator>();
         input = GetComponent<PlayerInput>();
         movementScript = GetComponent<PlayerMovement>();
     }
@@ -20,26 +22,32 @@ public class PlayerStats : MonoBehaviour
     private void Update()
     {
         // Death
-        if (health == 0)
+        if (health <= 0 && !anim.GetBool("Dead"))
         {
-            health = -1;
+            health = 0;
 
             input.canInput = false;
-
             input.KillInput();
 
             input.respawnOverride = true;
+
+            anim.SetBool("Dead", true);
+            anim.SetTrigger("Died");
         }
 
         // Respawn
         if (input.inputRespawn)
         {
-            transform.position = Vector3.zero;
-
             health = maxHeath;
 
-            input.respawnOverride = false;
             input.canInput = true;
+
+            input.respawnOverride = false;
+
+            anim.SetBool("Dead", false);
+            anim.SetTrigger("Revived");
+
+            transform.position = Vector3.zero;
         }
     }
 
