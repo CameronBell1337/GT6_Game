05f7141ec8cdda60f9aef8c0d779d35910a088dd
Unknown <Gabriel2.Vulpe@live.uwe.ac.uk>
2020-04-20 22:40:03 +0100

    Added attack UI

---
+++ GPP_To_The_Core/Assets/Pastures-Assets/Scripts/AttackTriggerText.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AttackTriggerText : MonoBehaviour
+{
+
+    private int counting = 0;
+    public Canvas text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && text.gameObject)
+        {
+            text.gameObject.SetActive(false);
+            counting ++;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("attackTrigger") && counting < 2)
+        {
+            text.gameObject.SetActive(true);
+        }
+    }
+}
