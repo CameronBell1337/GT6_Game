088388a23385238c90b813c44bc317bbfd67b143
Unknown <Gabriel2.Vulpe@live.uwe.ac.uk>
2020-04-21 01:30:59 +0100

    Adjusting the terrain, adding the cave

--- GPP_To_The_Core/Assets/Pastures-Assets/Scripts/Tutorial.cs
+++ GPP_To_The_Core/Assets/Pastures-Assets/Scripts/Tutorial.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
     public Canvas text;
+    private int counting = 0;
+
+    public Canvas run;
     // Start is called before the first frame update
     void Start()
     {
         text.gameObject.SetActive(true);
+        run.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,8 +23,22 @@ public class Tutorial : MonoBehaviour
         {
           
             text.gameObject.SetActive(false);
+            if (counting < 1)
+            {
+              run.gameObject.SetActive(true);  
+              counting = 1 ;
+            }
+            
         }
         
-        
+        if (Input.GetKey(KeyCode.LeftShift) )
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) ||
+                Input.GetKeyDown(KeyCode.D))
+            {
+                run.gameObject.SetActive(false);
+            }
+            
+        }
     }
 }
