75e9483d44769e587245917376af5b04db3409f6
Unknown <Gabriel2.Vulpe@live.uwe.ac.uk>
2020-04-21 04:05:15 +0100

    Added power-up

---
+++ GPP_To_The_Core/Assets/Pastures-Assets/Scripts/PowerUpCollect.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpCollect : MonoBehaviour
+{
+    public Canvas powerUpUses;
+
+    public static bool powerUpCollected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerUpUses.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (powerUpCollected && PowerUpEffect.counting != 0)
+        { 
+            powerUpUses.gameObject.SetActive(true);
+        }
+
+        if (PowerUpEffect.counting == 0)
+        {
+           StartCoroutine(Time()); 
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            Destroy(other.gameObject);
+            powerUpCollected = true;
+        }
+    }
+    
+    IEnumerator Time()
+    {
+        yield return new WaitForSeconds(5);
+        powerUpUses.gameObject.SetActive(false);
+    }
+}
---
+++ GPP_To_The_Core/Assets/Pastures-Assets/Scripts/PowerUpEffect.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpEffect : MonoBehaviour
+{
+    public float power = 15.0f;
+
+    public float radius = 5.0f;
+
+    public float upForce = 1.0f;
+
+    public static int counting = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.G) && counting > 0 && PowerUpCollect.powerUpCollected)
+        {
+            StartCoroutine(time());
+            Detonate();
+        }
+    }
+
+    void Detonate()
+    {
+        Vector3 explosionPosition = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+        foreach (Collider hit in colliders)
+
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null && hit.CompareTag("Enemy"))
+            {
+             rb.AddExplosionForce(power, explosionPosition, radius, upForce, ForceMode.Impulse);   
+            }
+            
+        }
+    }
+
+    IEnumerator time()
+    {
+        if (counting == 3)
+        {
+            counting = 2;
+        }else if (counting == 2)
+        {
+            counting = 1;
+        }else if (counting == 1)
+        {
+            counting = 0;
+        }
+        yield return new WaitForSeconds(0.5f);
+    }
+}
