c6a428e8c2162ecebae4d2986c8ccdbe9d111f44
Cameron <cameron2.bell@live.uwe.ac.uk>
2020-04-21 15:19:12 +0100

    Fixed CameraManager Bug
    mainVCam would not disable when cutscene would activate causing problems if the priority of the mainVCam was too high would not allow the camera to switch.

--- GPP_To_The_Core/Assets/ScriptsTOUse/cameraManager.cs
+++ GPP_To_The_Core/Assets/ScriptsTOUse/cameraManager.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class cameraManager : MonoBehaviour
 {
-    public GameObject player;
-    //public bool anotherCameraActive;
+   
+    
     public bool cutScene01Active;
     public bool cutScene02Active;
     public bool cutScene03Active;
     public bool cutScene04Active;
     public bool cutScene05Active;
 
+    private bool anotherCameraActive;
 
     public GameObject mainVCamera;
     public GameObject[] vCams;
 
     void Start()
     {
-        mainVCamera.SetActive(true);
-        //anotherCameraActive = false;
+        //mainVCamera.SetActive(true);
+
+        anotherCameraActive = false;
+
         cutScene01Active = false;
         cutScene02Active = false;
         cutScene03Active = false;
@@ -27,19 +30,41 @@ public class cameraManager : MonoBehaviour
         cutScene05Active = false;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        if(cutScene01Active || cutScene02Active || cutScene03Active || cutScene04Active || cutScene05Active)
+        {
+            anotherCameraActive = true;
+        }
+        else if (!cutScene01Active || !cutScene02Active || !cutScene03Active || !cutScene04Active || !cutScene05Active)
+        {
+            anotherCameraActive = false;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(anotherCameraActive)
+        {
+            mainVCamera.SetActive(false);
+        }
+        
+        if(!anotherCameraActive)
+        {
+            mainVCamera.SetActive(true);
+        }
+
         if (cutScene01Active)
         {
             vCams[0].SetActive(cutScene01Active);
             vCams[1].SetActive(cutScene01Active);
             vCams[2].SetActive(cutScene01Active);
-            mainVCamera.SetActive(false);
+            //mainVCamera.SetActive(false);
         }
         else
         {
-            mainVCamera.SetActive(true);
+            //mainVCamera.SetActive(true);
             vCams[0].SetActive(cutScene01Active);
             vCams[1].SetActive(cutScene01Active);
             vCams[2].SetActive(cutScene01Active);
@@ -47,64 +72,64 @@ public class cameraManager : MonoBehaviour
 
         if (cutScene02Active)
         {
-            vCams[3].SetActive(cutScene01Active);
-            vCams[4].SetActive(cutScene01Active);
-            vCams[5].SetActive(cutScene01Active);
-            mainVCamera.SetActive(false);
+            vCams[3].SetActive(cutScene02Active);
+            vCams[4].SetActive(cutScene02Active);
+            vCams[5].SetActive(cutScene02Active);
+            //mainVCamera.SetActive(false);
         }
         else
         {
-            mainVCamera.SetActive(true);
-            vCams[3].SetActive(cutScene01Active);
-            vCams[4].SetActive(cutScene01Active);
-            vCams[5].SetActive(cutScene01Active);
+            //mainVCamera.SetActive(true);
+            vCams[3].SetActive(cutScene02Active);
+            vCams[4].SetActive(cutScene02Active);
+            vCams[5].SetActive(cutScene02Active);
 
         }
 
         if (cutScene03Active)
         {
-            vCams[6].SetActive(cutScene01Active);
-            vCams[7].SetActive(cutScene01Active);
-            vCams[8].SetActive(cutScene01Active);
-            mainVCamera.SetActive(false);
+            vCams[6].SetActive(cutScene03Active);
+            vCams[7].SetActive(cutScene03Active);
+            vCams[8].SetActive(cutScene03Active);
+            //mainVCamera.SetActive(false);
         }
         else 
         {
-            mainVCamera.SetActive(true);
-            vCams[6].SetActive(cutScene01Active);
-            vCams[7].SetActive(cutScene01Active);
-            vCams[8].SetActive(cutScene01Active);
+            //mainVCamera.SetActive(true);
+            vCams[6].SetActive(cutScene03Active);
+            vCams[7].SetActive(cutScene03Active);
+            vCams[8].SetActive(cutScene03Active);
         }
 
         if (cutScene04Active)
         {
-            vCams[9].SetActive(cutScene01Active);
-            vCams[10].SetActive(cutScene01Active);
-            vCams[11].SetActive(cutScene01Active);
-            mainVCamera.SetActive(false);
+            vCams[9].SetActive(cutScene04Active);
+            vCams[10].SetActive(cutScene04Active);
+            vCams[11].SetActive(cutScene04Active);
+            //mainVCamera.SetActive(false);
         }
         else
         {
-            mainVCamera.SetActive(true);
-            vCams[9].SetActive(cutScene01Active);
-            vCams[10].SetActive(cutScene01Active);
-            vCams[11].SetActive(cutScene01Active);
+            //mainVCamera.SetActive(true);
+            vCams[9].SetActive(cutScene04Active);
+            vCams[10].SetActive(cutScene04Active);
+            vCams[11].SetActive(cutScene04Active);
 
         }
 
         if (cutScene05Active)
         {
-            vCams[12].SetActive(cutScene01Active);
-            vCams[13].SetActive(cutScene01Active);
-            vCams[14].SetActive(cutScene01Active);
-            mainVCamera.SetActive(false);
+            vCams[12].SetActive(cutScene05Active);
+            vCams[13].SetActive(cutScene05Active);
+            vCams[14].SetActive(cutScene05Active);
+            //mainVCamera.SetActive(false);
         }
         else 
         {
-            mainVCamera.SetActive(true);
-            vCams[12].SetActive(cutScene01Active);
-            vCams[13].SetActive(cutScene01Active);
-            vCams[14].SetActive(cutScene01Active);
+            //mainVCamera.SetActive(true);
+            vCams[12].SetActive(cutScene05Active);
+            vCams[13].SetActive(cutScene05Active);
+            vCams[14].SetActive(cutScene05Active);
 
         }
     }
